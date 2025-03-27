aefccb1248f0cf05ec30c46e9cdb0722b5e6e0e0
A1Gard <a1gard@4xmen.ir>
2025-03-28 02:51:43 +0330

    added name fixer

--- resources/views/segments/index/WTFIndex/WTFIndex.blade.php
+++ resources/views/segments/index/WTFIndex/WTFIndex.blade.php
@@ -4,19 +4,20 @@
         @foreach(getCategoriesSet($data->area_name.'_'.$data->part.'_categories') as $mainCategory)
             <div class="col" style="background: {{$mainCategory->bg_color}}; color: {{$mainCategory->color}}"
                  data-id="#wtf-{{$mainCategory->id}}">
-                {{$mainCategory->name}}
+                {{explode(' ',$mainCategory->name)[0]}}
             </div>
         @endforeach
     </div>
     <div class="py-4">
         @foreach(getCategoriesSet($data->area_name.'_'.$data->part.'_categories') as $k => $mainCategory)
+            @php($x = explode(' ',$mainCategory->name))
             <div class="{{gfx()['container']}} wtf-section" id="wtf-{{$mainCategory->id}}"  @if($k == 0) style="display: block" @endif>
                 <div class="row">
                     @foreach($mainCategory->children as $childCategory)
                         <div class="col-3">
                             <img src="{{$childCategory->imgUrl()}}" alt="{{$childCategory->name}}" >
                             <h5>
-                                {{$childCategory->name}}
+                                {{implode(' ',array_diff(explode(' ',$childCategory->name),$x))}}
                             </h5>
                         </div>
                     @endforeach
