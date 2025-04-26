008e8c4e23cc9ddba8bc02b46d20cefea6cbf3c3
A1gard <a1gard@4xmen.ir>
2025-04-26 03:33:51 +0330

    wtf 2

--- resources/views/segments/index/NeginNews/NeginNews.js
+++ resources/views/segments/index/NeginNews/NeginNews.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded',function () {
    document.querySelectorAll('.NeginNews li').forEach(function (el) {
-       if (el.innerHTML.indexOf('--')){
+       if (el.innerHTML.indexOf('--') !== -1){
            el.innerHTML = el.innerHTML.split('--').join('<span class="nsp"></span>')
        }
    })
