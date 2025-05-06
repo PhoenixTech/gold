94b81a7fe3a46e731b3ee6d370bfe39c4b064e49
A1Gard <a1gard@4xmen.ir>
2025-05-06 23:00:49 +0330

    fixed footer

--- resources/views/segments/footer/WTFFooter/WTFFooter.blade.php
+++ resources/views/segments/footer/WTFFooter/WTFFooter.blade.php
@@ -1,6 +1,9 @@
 <section class="WTFFooter live-setting" data-live="{{$data->area_name.'_'.$data->part}}" >
-    @foreach(getCategoriesSet($data->area_name.'_'.$data->part.'_categories') as $mainCategory)
+    @foreach(getCategoriesSet($data->area_name.'_'.$data->part.'_categories') as $k => $mainCategory)
         <a class="wtfooter-btn" href="{{$mainCategory->webUrl()}}" >
+            @if($k == 3)
+                <img id="ballon" src="{{asset('assets/default/ballon.webp')}}" alt="">
+            @endif
             <img src="{{$mainCategory->svgUrl()}}" alt="{{$mainCategory->name}}">
             {{$mainCategory->name}}
         </a>
