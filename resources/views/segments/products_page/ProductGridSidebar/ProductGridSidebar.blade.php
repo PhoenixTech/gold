1023e5d7ee3679aee70fbcbd2600f43edd1e6e8b
SadeghPM <SadeghPM@users.noreply.github.com>
2026-08-06 18:40:37 +0330

    style(category): update product grid cards across category pages with modern card UI

--- resources/views/segments/products_page/ProductGridSidebar/ProductGridSidebar.blade.php
+++ resources/views/segments/products_page/ProductGridSidebar/ProductGridSidebar.blade.php
@@ -1,27 +1,27 @@
-<section class='ProductGridSidebar content live-setting' data-live="{{$data->area_name.'_'.$data->part}}" id="product-list-view">
+<section class='ProductGridSidebar content live-setting py-4' data-live="{{$data->area_name.'_'.$data->part}}" id="product-list-view">
     <div class="{{gfx()['container']}}">
-        <h1>
-            {{$title}}
-        </h1>
-        <div class="row">
-            @if(!getSetting($data->area_name.'_'.$data->part.'_invert'))
-                <div class="col-lg-3 p-lg-1 pt-lg-0">
+        <div class="row g-4">
+            @if(getSetting($data->area_name.'_'.$data->part.'_invert'))
+                <div class="col-lg-4 col-xl-3">
                     @include('segments.products_page.ProductGridSidebar.inc.product-sidebar')
                 </div>
             @endif
-            <div class="col-lg-9">
-                <div class="row">
+
+            <div class="col-lg-8 col-xl-9">
+                <div class="row g-3 g-md-4">
                     @foreach($products as $product)
-                        <div class="col-md-4 p-2">
+                        <div class="col-6 col-md-6 col-lg-4">
                             @include(\App\Models\Area::where('name','product-grid')->first()->defPart(),compact('product'))
                         </div>
                     @endforeach
                 </div>
-                {{$products->withQueryString()->links()}}
-
+                <div class="mt-4">
+                    {{$products->withQueryString()->links()}}
+                </div>
             </div>
-            @if(getSetting($data->area_name.'_'.$data->part.'_invert'))
-                <div class="col-lg-3">
+
+            @if(!getSetting($data->area_name.'_'.$data->part.'_invert'))
+                <div class="col-lg-4 col-xl-3">
                     @include('segments.products_page.ProductGridSidebar.inc.product-sidebar')
                 </div>
             @endif
